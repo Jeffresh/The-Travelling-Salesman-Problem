6bba202d1db9d0523dee1b5b6b2654fe04611f06
jeffresh <jeff.uca@gmail.com>
2019-05-20 13:06:13 +0200

    deleted first and last spaces

--- main.cpp
+++ main.cpp
@@ -9,12 +9,18 @@ int main()
     std::ifstream myfile("TSPLIB/a280.tsp/a280.tsp");
 
     regex number("((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?((e|E)((\\+|-)?)[[:digit:]]+)?");
-    regex word("[[:alpha:]]+(_)*[[:space:]]*(:)*(\\([[:space:]]*[[:alpha:]]*[[:space:]]*\\))*");
-    regex more_space("(([[:space:]][[:space:]])[[:space:]]*)\t*");
+    regex word("[[:alpha:]]+(_)*[[:space:]]*(:)*(\\([[:space:]]*[[:alpha:]]+[[:space:]]*\\))*");
+    
+
+    
+
+    regex more_space("(([[:space:]])[[:space:]]*)\t*");
     string clean_numbers,clean_words,line;
     //Replace with an empty string
     const string format="";
     const string format2=" ";
+
+    string numbers;
 
     if(myfile.is_open())
     {
@@ -22,9 +28,26 @@ int main()
         while(getline (myfile,line) )
         {
             clean_words=regex_replace(line,word,format,regex_constants::format_default);
+
+
             clean_words=regex_replace(clean_words,more_space,format2,regex_constants::format_default);
 
-            cout<<clean_words<<endl;
+
+            
+	     
+
+            clean_words = std::regex_replace(clean_words, std::regex("^\\s+"), std::string(""));
+            clean_words = std::regex_replace(clean_words, std::regex("\\s+$"), std::string(""));
+            // clean_words = std::regex_replace(clean_words, std::regex("\n+$"), std::string(""));
+
+            numbers += clean_words+"\n";
+
+        
+        }
+
+        for(auto number:numbers)
+        {
+            cout<<number;
         }
 
 
